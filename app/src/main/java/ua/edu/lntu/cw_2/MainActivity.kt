2c95f6987032ebe5712534a3789a58602ef0ad71
LaNcIaToR <jonas7238@icloud.com>
2024-03-12 13:16:44 +0200

    deleted a part of template code

--- app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw_2/MainActivity.kt
@@ -29,18 +29,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    IPZ_CW_2_Zhuk_IlliaTheme {
-        Greeting("Android")
-    }
-}
